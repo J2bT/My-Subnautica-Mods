58673789e9c69f4b0c339f2dccff073772564dce
J2bT <67165942+J2bT@users.noreply.github.com>
2022-12-25 12:27:42 +0300

    Project configured.
    Used basic configuration without SN / BZ separation. There's no need because Below Zero mods are still using QModManager.

--- ControlChipMod_BepInEx/Properties/AssemblyInfo.cs
+++ ControlChipMod_BepInEx/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ControlChipMod_BepInEx")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("J2bT_ControlChipMod")]
+[assembly: AssemblyDescription("J2bT's first subnautica mod.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ControlChipMod_BepInEx")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("J2bT.ControlChipMod")]
+[assembly: AssemblyCopyright("Copyright ©  2022 J2bT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
