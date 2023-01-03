e4331eeab2e823e3a45c754763175e9b06b0c606
J2bT <67165942+J2bT@users.noreply.github.com>
2023-01-03 21:35:41 +0300

    Dependency attribute added.
    Plugin will no longer try to load unless SMLHelper is present.

--- ControlChipMod/EntryPoints/Plugin.cs
+++ ControlChipMod/EntryPoints/Plugin.cs
@@ -4,6 +4,7 @@ using BepInEx;
 namespace J2bT.ControlChipMod.EntryPoints
 {
     [BepInPlugin("com.j2bt.controlchipmod", Main.modName, Main.versionString)]
+    [BepInDependency("com.ahk1221.smlhelper")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
