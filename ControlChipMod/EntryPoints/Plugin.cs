a3c7437f56828cb2833b6e9c25e708b9c96be6f6
J2bT <67165942+J2bT@users.noreply.github.com>
2023-01-06 02:21:18 +0300

    Minimum required SMLHelper's ver set to 2.15.0.1

--- ControlChipMod/EntryPoints/Plugin.cs
+++ ControlChipMod/EntryPoints/Plugin.cs
@@ -4,7 +4,7 @@ using BepInEx;
 namespace J2bT.ControlChipMod.EntryPoints
 {
     [BepInPlugin("com.j2bt.controlchipmod", Main.modName, Main.versionString)]
-    [BepInDependency("com.ahk1221.smlhelper")]
+    [BepInDependency("com.ahk1221.smlhelper", "2.15.0.1")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
