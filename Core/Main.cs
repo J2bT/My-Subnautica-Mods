160a1e907d7c8620cd3d7372f37d8f0711f29c77
J2bT <67165942+J2bT@users.noreply.github.com>
2023-01-20 21:56:54 +0300

    New configurations and core mod.

---
+++ Core/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J2bT.Core
+{
+    internal class Main
+    {
+    }
+}
